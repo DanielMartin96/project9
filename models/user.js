36dd96e855740dd76b2f8bff05da160bae28b809
DanielMartin96 <danielmartin1996@hotmail.com>
2021-02-04 11:50:59 +0000

    Added foreign key to user model

--- models/user.js
+++ models/user.js
@@ -70,7 +70,9 @@ module.exports = (sequelize) => {
   );
 
   User.associate = (models) => {
-    User.hasMany(models.Course);
+    User.hasMany(models.Course, {
+      foreignKey: "userId",
+    });
   };
 
   return User;
